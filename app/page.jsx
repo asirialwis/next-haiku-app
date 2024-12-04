27c61f1cfa482413c90925b565363db8e56e180b
asirialwis <asirichathurika900@gmail.com>
2024-12-04 18:42:49 +0530

    logged user identify

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
 import RegisterForm from "../components/RegisterForm";
-export default function Page() {
+import { getUserFromCookie } from "../lib/getUser";
+
+export default async function Page() {
+  const user = await getUserFromCookie();
+
   return (
     <>
-      <p className="text-center text-2xl text-grey-600 mb-5">
-        Don&rsquo;t have an account <strong>Create One</strong>
-      </p>
-      <RegisterForm/>
+      {user && <p>Welcome, You are logged in.</p>}
+      {!user && (
+        <>
+          <p className="text-center text-2xl text-grey-600 mb-5">
+            Don&rsquo;t have an account <strong>Create One</strong>
+          </p>
+          <RegisterForm />
+        </>
+      )}
     </>
   );
 }
